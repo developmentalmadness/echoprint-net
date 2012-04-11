099017c34dd992d3a89662957fdd6d9a6811c5a1
Mark J. Miller <developmentalmadness@gmail.com>
2012-04-11 07:16:00 -0600

    EchonestAPI.IdentifySong is working

---
+++ src/echoprint-net/Data/response.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace echoprint_net.Data
+{
+    /*
+     {
+  "response": {
+    "status": {
+      "code": 0,
+      "message": "Success",
+      "version": "4.2"
+    },
+    "songs": [
+      {
+        "title": "Billie Jean",
+        "artist_name": "Michael Jackson",
+        "artist_id": "ARXPPEY1187FB51DF4",
+        "score": 49,
+        "message": "OK (match type 5)",
+        "id": "SOKHYNL12A8C142FC7"
+      }
+    ]
+  }
+}*/
+    public class song
+    {
+        public string title { get; set; }
+        public string artist_name { get; set; }
+        public string artist_id { get; set; }
+        public int score { get; set; }
+        public string message { get; set; }
+        public string id { get; set; }
+    }
+
+    public class status
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public string version { get; set; }
+    }
+
+    public class response
+    {
+        public status status { get; set; }
+        public List<song> songs { get; set; }
+    }
+}
---
+++ src/echoprint-net/EchonestAPI.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using echoprint_net.Data;
+using Newtonsoft.Json.Linq;
+
+namespace echoprint_net
+{
+    public enum EchonestResult
+    {
+        None,
+        Error,
+        NotFound,
+        Success
+    }
+
+    class EchonestAPI
+    {
+        public EchonestResult IdentifySong(Code item, out String errorMessage)
+        {
+            EchonestResult result = EchonestResult.None;
+            errorMessage = String.Empty;
+
+            while (result == EchonestResult.None)
+            {
+                var api_key = ConfigurationManager.AppSettings["echonest_api_key"];
+                var url = String.Format("http://developer.echonest.com/api/v4/song/identify?api_key={0}", api_key);
+
+                var request = WebRequest.Create(url) as HttpWebRequest;
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.Method = "POST";
+
+                JObject query = JObject.FromObject(new
+                {
+                    code = item.code,
+                    metadata = new
+                    {
+                        version = decimal.Parse(item.metadata.version)
+                    }
+                });
+
+                StringBuilder body = new StringBuilder();
+                body.AppendFormat("version=3.15&query={0}", query);
+
+                var buffer = Encoding.UTF8.GetBytes(body.ToString());
+
+                try
+                {
+                    var output = String.Empty;
+                    using (var input = request.GetRequestStream())
+                    {
+                        input.Write(buffer, 0, buffer.Length);
+
+                        var response = request.GetResponse() as HttpWebResponse;
+                        try
+                        {
+                            using (var outstream = response.GetResponseStream())
+                            using (StreamReader reader = new StreamReader(outstream))
+                                output = reader.ReadToEnd();
+                        }
+                        finally
+                        {
+                            if (response != null)
+                                response.Close();
+                        }
+                    }
+
+                    var j = JObject.Parse(output);
+
+                    if (j["response"]["status"]["code"].Value<int>() == 0
+                        && j["response"]["songs"].Count() == 1)
+                    {
+                        var song = j["response"]["songs"][0];
+                        item.metadata.id = song["id"].Value<string>().Trim();
+                        item.metadata.artist_id = song["artist_id"].Value<string>().Trim();
+                        item.metadata.title = song["title"].Value<string>().Trim();
+                        item.metadata.artist = song["artist_name"].Value<string>().Trim();
+                        result = EchonestResult.Success;
+                    }
+                    else
+                        result = EchonestResult.NotFound;
+
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Response != null && ex.Response.Headers["X-RateLimit-Remaining"] == "0")
+                    {
+                        // slow things down and retry
+                        Thread.Sleep(10000);
+                    }
+                    else
+                    {
+                        result = EchonestResult.Error;
+                        errorMessage = ex.Message;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    result = EchonestResult.Error;
+                    errorMessage = ex.Message;
+                }
+            }
+
+            return result;
+        }
+    }
+}
